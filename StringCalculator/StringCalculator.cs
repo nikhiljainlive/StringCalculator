a49f60ed8dd54056edf3b562bda056d020acdcd1
nikhiljainlive <nikhiljain5195@gmail.com>
2021-06-29 00:30:33 +0530

    Refactored StringCalculator by removing redundant lines and if check

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -2,32 +2,30 @@
 {
     public class StringCalculator
     {
-        public static char DELIMITER_COMMA = ',';
-        public static char DELIMITER_NEW_LINE = '\n';
+        private static string DELIMITER_COMMA = ",";
+        private static string DELIMITER_NEW_LINE = "\n";
 
         public int Add(string numberString)
         {
             if (IsStringEmpty(numberString))
                 return 0;
 
-            if (numberString.Contains(DELIMITER_COMMA.ToString()) || numberString.Contains(DELIMITER_NEW_LINE.ToString())) {
-                string[] tokens = ConvertToTokens(numberString);
-                int result = 0;
+            string[] tokens = ConvertToTokens(numberString);
+            int result = 0;
 
-                foreach (string token in tokens) {
-                    result += StringToInt(token);
-                }
-
-                return result;
+            foreach (string token in tokens)
+            {
+                result += StringToInt(token);
             }
 
-            return StringToInt(numberString);
+            return result;
         }
 
         private bool IsStringEmpty(string input) => input.Length == 0;
 
         private int StringToInt(string input) => int.Parse(input);
 
-        private string[] ConvertToTokens(string input) => input.Split(new char[] { DELIMITER_COMMA, DELIMITER_NEW_LINE });
+        private string[] ConvertToTokens(string input) => input.Split(new string[]
+        { DELIMITER_COMMA, DELIMITER_NEW_LINE }, System.StringSplitOptions.None);
     }
 }
